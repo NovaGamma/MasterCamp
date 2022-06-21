3ccaff5c82dc1f747d79198d65bae4c4f2c5bca0
Elvin Aupiais--Berthy <elvin.aupiais-berthy@efrei.net>
2022-06-21 10:51:58 +0200

    add routes for user and commune

--- Server/routes/comment.routes.js
+++ Server/routes/comment.routes.js
@@ -1,35 +1,34 @@
 'use strict';
 
- module.exports = function(app) {
- let commentsList = require('../controllers/comment.controller');
- let userHandlers = require('../controllers/authController');
+module.exports = function(app) {
+  let userHandlers = require('../controllers/authController');
+  let communeHandlers = require('../controllers/communeController');
 
- // commentList Routes
+  app.route("/commune/create")
+    .post(communeHandlers.create);
 
- // get and post request for /comments endpoints
-  app.route("/api/comments")
-    .get(commentsList.findAll)
-  app.route("/api/create_comment")
-    .post(userHandlers.loginRequired, commentsList.create); // update with login handler
-  app.route("/api/comment")
-    .get(userHandlers.loginRequired, commentsList.findOne); // update with login handler
-  app.route("/api/update_comment")
-    .post(userHandlers.loginRequired, commentsList.update); // update with login handler
-  app.route("/api/delete_comment")
-    .post(userHandlers.loginRequired, commentsList.delete); // update with login handler
-  app.route("/api/delete_comments")
-    .post(userHandlers.loginRequired, commentsList.deleteAll); // update with login handler
-  app.route("/api/comments_by_name")
-    .get(commentsList.findByName); // update with login handler
+  app.route("/commune/findAll")
+    .get(communeHandlers.findAll);
+
+   //app.route("/api/delete_comments")
+   //  .post(userHandlers.loginRequired, commentsList.deleteAll); // update with login handler
+   //app.route("/api/comments_by_name")
+   //  .get(commentsList.findByName); // update with login handler
 
 
-/// rest of request defined in Routes politics
+ // rest of request defined in Routes politics
 
- // post request for user registration
- app.route("/auth/register")
-  .post(userHandlers.register);
+  // post request for user registration
+  app.route("/auth/register")
+   .post(userHandlers.register);
 
- // post request for user log in
- app.route("/auth/login")
-  .post(userHandlers.signIn);
-}
+  app.route("/auth/findAll")
+    .get(userHandlers.findAll);
+
+  app.route("/auth/delete")
+    .post(userHandlers.delete);
+
+  // post request for user log in
+  app.route("/auth/login")
+   .post(userHandlers.signIn);
+} 
