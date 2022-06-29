3fe713bbb6eaf99ca56cbc101102060299518415
Eric-Wang159 <73349489+Eric-Wang159@users.noreply.github.com>
2022-06-29 12:20:21 +0200

    update page validation
    update page validation

--- vueClient/src/components/Validation.vue
+++ vueClient/src/components/Validation.vue
@@ -1,5 +1,13 @@
 <template>
-  salut
+  <section class="containerValid">
+
+    <div class="textValid">
+      <h3 class="DescValid">Votre compte est en attente de validation par la mairie de votre commune.<br><br>
+        Vous pourrez ensuite vous connecter lorsque celui-ci sera validé</h3>
+    </div>
+
+
+  </section>
 </template>
 
 <script>
@@ -9,5 +17,30 @@ export default {
 </script>
 
 <style scoped>
+
+.containerValid{
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  background-color: #F2F2F2;
+}
+
+.textValid{
+  display: flex;
+  margin-top: 7%;
+  background-color: white;
+  width: 40%;
+  height: 500px;
+  justify-content: center;
+  box-shadow: 0px 0px 10px 3px darkgrey;
+  border-radius: 40px;
+}
+
+.DescValid{
+  margin: 10%;
+  font-family: Arial;
+  font-size: x-large;
+  align-self: center;
+}
 
 </style>
